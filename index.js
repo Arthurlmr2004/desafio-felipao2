5d1ec6bb87427fa51b4dac8e3038924affc1d8c2
Arthur Larangeiras <arthurlarangeiras@gmail.com>
2023-10-27 11:58:37 -0300

    2 desafio concluido

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-function teste(victory, defeat) {
+function calculateWinLossBalance(victory, defeat) {
     let result = victory - defeat;
     return result;
 }
 
-function main() {
-    let victory = 50, defeat = 20;
-    teste(victory, defeat);
-    console.log("O heroi tem saldo de:");
+function getRank(victory, defeat) {
+    const balance = calculateWinLossBalance(victory, defeat);
+    let rank = "";
+
+    if (balance < 10) {
+        rank = "Ferro";
+    } 
+    else if (balance >= 11 && balance <= 20) {
+        rank = "Bronze";
+    }
+     
+    else if (balance >= 21 && balance <= 50) {
+        rank = "Prata";
+    }
+     
+    else if (balance >= 51 && balance <= 80) {
+        rank = "Ouro";
+    }
+     
+    else if (balance >= 81 && balance <= 90) {
+        rank = "Diamante";
+    }
+     
+    else if (balance >= 91 && balance <= 100) {
+        rank = "Lendário";
+    }
+     
+    else {
+        rank = "Imortal";
+    }
+
+    return rank;
 }
+
+const victories = 90;
+const defeats = 10;
+const balance = calculateWinLossBalance(victories, defeats);
+const heroRank = getRank(victories, defeats);
+
+console.log(`O Herói tem saldo de ${balance} e está no nível de ${heroRank}`);
