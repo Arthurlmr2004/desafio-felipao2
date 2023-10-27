2f2fd168824a2064d12215a786d4f6d21ebdee6c
Arthur Larangeiras <arthurlarangeiras@gmail.com>
2023-10-27 11:37:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function teste(victory, defeat) {
+    let result = victory - defeat;
+    return result;
+}
+
+function main() {
+    let victory = 50, defeat = 20;
+    teste(victory, defeat);
+    console.log("O heroi tem saldo de:");
+}
